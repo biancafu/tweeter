04b33a0ac62d70f8fa88c39d510744186486205a
Bianca Fu <0509biancafu@gmail.com>
2022-12-09 01:52:16 +0000

    added a comment to explain why I chose to post new tweet by using Ajax instead of clearing the container and only adding new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -42,9 +42,7 @@ $(document).ready(function () {
             $(".counter").val('140');
           })
 
-        //since clearing out the tweets-container is visible when loading,
-        //instead of clearing the whole tweet section and calling loadtweets,
-        //I decided to get only the new tweet so that the clearing is not visible. below is the code that clears the tweet:
+        //when clearing out the tweets-container, the cut off is visible while loading, instead of clearing the whole tweet section and calling loadtweets,I decided to get only the new tweet so that the clearing is not visible and it seems load be faster as well. below is the code that clears the tweet:
 
         // $(".tweets-container").empty();
         // $("#tweet-text").val('');
